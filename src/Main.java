c277c616bd3fc18e68267e20a8e2820f2f4ee5e1
Laurynas <laurynas.ubys@gmail.com>
2021-03-25 13:49:04 +0100

    Added tasks for deploying jar and starting a debug server on rpi

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello java");
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("Start");
+
+        while (true) {
+            Thread.sleep(1000);
+            System.out.println("I really need to chill out now");
+        }
     }
 }
